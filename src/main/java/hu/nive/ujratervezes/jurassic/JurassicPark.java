0d7ea35e35cf158ee25c12c146cf59f31610d927
mozes <mozes.bence@gmail.com>
2021-04-29 12:28:58 +0200

    finished

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,15 +1,45 @@
 package hu.nive.ujratervezes.jurassic;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JurassicPark {
 
     private String dbUrl;
     private String dbUser;
     private String dbPassword;
+    private final Connection conn;
 
     public JurassicPark(String dbUrl, String dbUser, String dbPassword) {
         this.dbUrl = dbUrl;
         this.dbUser = dbUser;
         this.dbPassword = dbPassword;
+        this.conn = getConnection();
     }
 
-}
+    private Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+        } catch (SQLException e) {
+            System.err.println("Database not reachable.");
+        }
+        return connection;
+    }
+    public List<String> checkOverpopulation () {
+        List<String> dinosaurs = new ArrayList<>();
+        try(this.conn) {
+            String query = "Select breed from dinosaur where actual > expected order by breed asc";
+            PreparedStatement ps = conn.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                dinosaurs.add(rs.getString(1));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return dinosaurs;
+    }
+    }
+
